f848a84fe3ede88e6e5abeb029f8dbf75db92c83
Joaquín Galasso <joacogalasso@gmail.com>
2024-05-28 11:32:48 -0300

    .

--- index.js
+++ index.js
@@ -1,25 +1,58 @@
-$(document).ready(function () {
-  $("#close").hide();
-  // Cuando se envía el formulario
-  $("#formulario").submit(function (e) {
-    // Obtenemos los valores de los campos
-    let nombre = $("#ingresa-nombre").val();
-    let email = $("#ingresa-mail").val();
+/*obtenemos los datos que queremos del formulario */
+const inputnick = document.getElementById('ingresa-nombre');
+const inputemail = document.getElementById('ingresa-mail');
+const inputcheck1 = document.getElementById('check-1');
+const inputcheck2 = document.getElementById('check-2');
+const inputcheck3 = document.getElementById('check-3');
+const inputcheck4 = document.getElementById('check-4');
+const inputcheck5 = document.getElementById('check-5');
+const inputcheck6 = document.getElementById('check-6');
 
-    console.log(nombre);
-    console.log(email);
+/*Puntero para el boton */
+const botonregform = document.getElementById('btn-form');
 
-    // Muestra el mensaje y oculta el formulario
-    $(".formMessage").show();
+/*lugar donde se va a cambiar la informacion */
+const NombreCard = document.getElementById('nombre-feedback');
+const MailCard = document.getElementById('mail-feedback');
 
-    // Previene el envío tradicional del formulario
-    e.preventDefault();
-  });
+/*obtenemos la tarjeta donde se vana  cambair los datos*/
+const divtarjeta = document.getElementById('modal-1');
 
 
-  // Obtener Placeholders
-var placeholder_nombre = document.getElementById("ingresa-nombre");
-var placeholder_email = document.getElementById("ingresa-mail");
+/*Botones para cerrar las tarjetas */
+const Btncerrar = document.getElementById('btn-cerrar');
+
+/*Bajar hasta el formulario */
+const btnbajar = document.getElementById('bajar-form');
+const seccionhasta = document.getElementById('hasta-aca');
+
+
+/*Vamos a cambiar la clase  para que se vea la tarjeta, poner los valores extraidos del formulario y que se esconda al cerrarla*/
+botonregform.addEventListener('click', function () {
+    const nick = inputnick.value
+    const mail = inputemail.value
+    NombreCard.textContent = nick ;
+    MailCard.textContent = mail;
+    document.body.classList.add("jw-modal-open");
+});
+
+
+/*Limpiamos los campos despues de cerrar el popup*/
+Btncerrar.addEventListener('click', function (e) {
+    document.body.classList.remove("jw-modal-open");
+    inputnick.value = ''
+    inputemail.value = ''
+    inputname.value = ''
+    inputlastna.value = ''
+    inputpass.value = ''
+});
+
+/*baja hasta la seccion del formulario*/
+btnbajar.addEventListener('click', function (e) {
+    seccionhasta.scrollIntoView({ behavior: 'smooth' })
+});
+
+
 
 
   // Cuando se hace clic en el botón cerrar
